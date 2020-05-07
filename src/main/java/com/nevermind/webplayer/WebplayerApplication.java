5818253643c18ee51ae203aaa8e84c25281684ef
yjin13 <bly0319@naver.com>
2020-05-07 14:54:48 +0900

    Initial commit

---
+++ src/main/java/com/nevermind/webplayer/WebplayerApplication.java
@@ -0,0 +1,13 @@
+package com.nevermind.webplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebplayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nevermind/webplayer/WebplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nevermind.webplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebplayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
